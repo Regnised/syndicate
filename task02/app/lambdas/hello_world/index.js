bc3111a49abd6866bede03c8c366a39555b9f1cf
Vasyl Onopriienko <2588425+Regnised@users.noreply.github.com>
2024-08-26 23:58:32 +0300

    fix response

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,14 +1,16 @@
 
 exports.handler = async (event) => {
   if (event.requestContext.http.path === '/hello' && event.requestContext.http.method === 'GET') {
+    const statusCode = 200;
     return {
-      statusCode: 200,
-      message: 'Hello from Lambda',
+      statusCode,
+      body: JSON.stringify({ statusCode, message: 'Hello from Lambda' }),
     };
   } else {
+    const statusCode = 400;
     return {
-      statusCode: 400,
-      body: JSON.stringify({ statusCode: 400, message: `Bad request syntax or unsupported method. Request path: ${event.requestContext.http.path}. HTTP method: ${event.requestContext.http.method}` })
+      statusCode,
+      body: JSON.stringify({ statusCode, message: `Bad request syntax or unsupported method. Request path: ${event.requestContext.http.path}. HTTP method: ${event.requestContext.http.method}` })
     };
   }
 };
